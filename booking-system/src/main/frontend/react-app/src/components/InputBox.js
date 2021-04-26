1e2d5887d4874c6f774a6d9a7544bfe6dcc3d2cd
alexandrugabrielrait <r_alexgaby@yahoo.com>
2021-04-27 01:35:28 +0300

    Fixed Frontend and HTTP requests

--- booking-system/src/main/frontend/react-app/src/components/InputBox.js
+++ booking-system/src/main/frontend/react-app/src/components/InputBox.js
@@ -5,49 +5,79 @@ import Proptypes from 'prop-types'
 import "./InputBox.css";
 import axios from "axios";
 
-const InputBox = ({buttonText}) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+const instance = axios.create({
+    baseURL: "http://localhost:8080",
+    withCredentials: false,
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+    }
+});
 
-  function validateForm() {
-    return email.length > 0 && password.length > 0;
-  }
+function SimpleGet () {
+    instance.get("/test")
+        .then((response) => {
+             console.log(response.data);
+        })
+        .catch(error => {
+            ""
+        })
+}
 
-  function handleSubmit(event) {
-    event.preventDefault();
+const InputBox = ({ buttonText }) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-    axios.post('user', {
-        email: email,
-        password: password
-    })
-  }
+    function validateForm() {
+        return email.length > 0 && password.length > 0;
+    }
 
-  return (
-    <div className="InputBox">
-      <Form onSubmit={handleSubmit}>
-        <Form.Group size="lg" controlId="email">
-          <Form.Label>Email</Form.Label><br/>
-          <Form.Control
-            autoFocus
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group size="lg" controlId="password">
-          <Form.Label>Password</Form.Label><br/>
-          <Form.Control
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </Form.Group>
-        <Button block size="lg" type="submit" disabled={!validateForm()}>
-          {buttonText}
-        </Button>
-      </Form>
-    </div>
-  );
+    function handleSubmit(event) {
+        //setEmail("CONSTANTIN");
+
+        event.preventDefault();
+
+        //setPassword(axios.get(`${API_URL}`));
+        /* , {
+            email: email,
+            password: password
+        }) */
+    }
+    axios.get("https://api.github.com/users/mapbox")
+        .then((response) => {
+            console.log(response.data);
+            console.log(response.status);
+            console.log(response.statusText);
+            console.log(response.headers);
+            console.log(response.config);
+        });
+
+    return (
+        <div className="InputBox">
+            <Form onSubmit={handleSubmit}>
+                <Form.Group size="lg" controlId="email">
+                    <Form.Label>Email</Form.Label><br />
+                    <Form.Control
+                        autoFocus
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </Form.Group>
+                <Form.Group size="lg" controlId="password">
+                    <Form.Label>Password</Form.Label><br />
+                    <Form.Control
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </Form.Group>
+                <Button block size="lg" type="submit" disabled={!validateForm()}>
+                    {buttonText}
+                </Button>
+            </Form>
+        </div>
+    );
 }
 
 InputBox.defaultProps = {
